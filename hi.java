d902eab5047f45738731ffc577fdc0ac7b8141e0
athira <akshara.v@cloudium.io>
2019-05-03 09:39:40 +0530

    added 2nd file

---
+++ hi.java
@@ -0,0 +1,19 @@
+public class hi {
+  public static void main(String []args)
+  {
+      System.out.println("hai java");
+      int num=5;
+      if(num>2){
+          if(num<5) {
+              System.out.println("num btw 2 and 5..");
+
+          }
+          else{System.out.println("ohoo");}
+      }
+      else if(num>1)
+      {
+          System.out.println("2..");
+      }
+      else{System.out.println("no..");}
+  }
+}
